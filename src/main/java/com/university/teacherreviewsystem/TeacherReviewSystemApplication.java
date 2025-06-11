417349f20238065a622c5b77c3373c085fc7b086
NiKiT0S1 <bobilevnikita88@gmail.com>
2025-06-11 14:00:02 +0500

    Initial comit

---
+++ src/main/java/com/university/teacherreviewsystem/TeacherReviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.university.teacherreviewsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherReviewSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherReviewSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/university/teacherreviewsystem/TeacherReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.teacherreviewsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherReviewSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
